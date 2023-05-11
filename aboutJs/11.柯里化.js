0365312d2e994ae3c0d3c22bd7f913e4bd19c3db
yang.wang <yang.wang@xtransfer.cn>
2023-05-11 23:04:46 +0800

    feat: 优化柯里化

--- aboutJs/11.柯里化.js
+++ aboutJs/11.柯里化.js
@@ -1,9 +1,9 @@
 // 柯里化
 function currying(fn, ...args) {
-    if (args.length >= fn.length) {
-        return fn(...args);
-    }
-    return (...args1) => {
-        currying(fn, ...args, ...args1);
-    }
+  return args.length >= fn.length
+    ? fn(...args)
+    : (...args1) => currying(fn, ...args, ...args1);
 }
+const add = (a, b, c) => a + b + c;
+const curried = currying(add);
+console.log(curried(2, 4)(3)); // 输出 6
