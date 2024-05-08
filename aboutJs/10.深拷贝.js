1b764ecbf9806863a8662a97864eb18be25e9f4f
yang.wang <1370228031@qq.com>
2024-05-08 16:24:14 +0800

    feat: 深拷贝优化

--- aboutJs/10.深拷贝.js
+++ aboutJs/10.深拷贝.js
@@ -1,34 +1,45 @@
-const deepClone = (obj, cached = new Set()) => {
-  const copy = Array.isArray(obj) ? [] : {};
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      // 如果是对象，并且缓存中没有改对象，才进行深拷贝
-      if (typeof obj[key] === "object" && !cached.has(obj[key])) {
-        // 把对象加入缓存中
-        cached.add(obj[key]);
-        copy[key] = deepClone(obj[key], cached);
-      } else {
-        copy[key] = obj[key];
-      }
-    }
+const deepClone = (obj, cached = new Map()) => {
+  if (obj instanceof RegExp) return new RegExp(obj);
+  if (obj instanceof Date) return new Date(obj);
+  // if (obj instanceof Function) return obj;
+  if (obj instanceof Function) {
+    return function () {
+      return obj.apply(this, arguments);
+    };
   }
+
+  if (typeof obj !== "object" && obj === null) return obj;
+
+  if (cached.has(obj)) return cached.get(obj);
+
+  const copy = Array.isArray(obj) ? [] : {};
+  cached.set(obj, copy);
+  Object.keys(obj).forEach((key) => {
+    copy[key] = deepClone(obj[key], cached);
+  });
   return copy;
 };
 
-const obj = {
-  name: "william",
-  age: 18,
-  address: {
-    city: "beijing",
-    street: "xizhimen",
+// 测试例子
+const obj1 = {
+  a: 1,
+  b: [2, 3],
+  c: { d: 4 },
+  e: new Date(),
+  f: /[a-z]/,
+  g: function () {
+    console.log("Function");
   },
-  hobbies: ["basketball", "football"],
-  family: obj,
 };
+obj1.h = obj1; // 添加循环引用
 
-console.log(obj);
-const copy = deepClone(obj);
-console.log(copy);
-copy.name = "梨花";
-console.log(obj);
-console.log(copy);
+const obj2 = deepClone(obj1);
+console.log(obj2);
+
+// 测试循环引用是否正确处理
+console.log(obj2.h === obj2); // true
+
+console.log(obj1.g === obj2.g); //???
+
+// 测试函数是否正确复制
+obj2.g(); // "Function"
